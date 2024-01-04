17d7a737193efa0478d78542f18dd176098e0423
kg2407 <56346039+kg2407@users.noreply.github.com>
2024-01-04 01:35:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversinoperations.js
@@ -0,0 +1,47 @@
+// let score = 33;
+// console.log(typeof score)
+
+// let score = "33";
+// console.log(typeof score)
+
+// let score = null;
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// let score = undefined;
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+
+// let score = true;
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// let score = false;
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// console.log(typeof NaN)
+
+//Conversion using Number
+
+// 33 => 33
+// "33" => 33
+// "33hj" => NaN
+// undefined => NaN
+// null => 0
+//true => 1
+//false => 0
+
+// Conversion to boolean
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false; "somestring" => true
