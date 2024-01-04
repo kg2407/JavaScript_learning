6d48c3052e6a60c08b7d84a86f02d0bce2c1b485
kg2407 <56346039+kg2407@users.noreply.github.com>
2024-01-04 06:47:51 +0000

    change the name

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,10 @@
+// Primitive
+
+// 7 types => String, Number, Boolean, BigInt, Symbol, null, undefined
+
+
+// Reference (Non primitive)
+
+// Object, Array and Function
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
