1216a1f16070703f5288b317cea31ee32e2b9935
kg2407 <56346039+kg2407@users.noreply.github.com>
2024-01-04 07:05:28 +0000

    Types of memry updated

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -8,3 +8,13 @@
 // Object, Array and Function
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+/*
+    Two types of memory
+    1. Stack => used to store primitive data types, here you get the copy to work with
+    2. Heap => used to store non primitive data types, here you get the reference to the memory
+
+    Primitives like numbers and booleans are stored by value, while objects and 
+    functions are stored by reference.
+*/
